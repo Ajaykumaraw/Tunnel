ca47fc92c997ea93cb90027aa3d1221a6e3e8262
ajaykumaraw <378ajaykumar@gmail.com>
2023-05-22 23:58:09 +0530

    code api setup

--- tunnel/app/api/code/route.js
+++ tunnel/app/api/code/route.js
@@ -1,1 +1,11 @@
 // path for this api -> http:localhost:3000/api/co
+
+
+export async function GET(request){
+    return new Response('Hello, Next js code');
+}
+                                                                                   
+
+export async function POST(request){
+   
+}
