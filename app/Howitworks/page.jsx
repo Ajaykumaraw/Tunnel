91170383464cb8975b7fadfb1ffb887fbae6e12d
ajaykumaraw <378ajaykumar@gmail.com>
2023-06-09 20:39:12 +0530

    how it works

---
+++ app/Howitworks/page.jsx
@@ -0,0 +1,15 @@
+const Howitworks = () => {
+  return (
+    <div className="app__howitworks w-full h-screen bg-slate-100">
+        <div className="app_howitworks-container w-2/6 h-screen bg-greeen-100">
+            <ul>
+              <li>Login in your account.</li>
+              <li>Generate Tunnel Code to use it.</li>
+              <li>Once tunnel code generated use the code to any other screen to connect.</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Howitworks
